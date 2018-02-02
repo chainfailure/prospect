26eb0a0f4acd415200e2c584f6325f50de251f0c
Chainfailure <chainfailure@protonmail.com>
2018-02-02 14:12:08 +0100

    some more whitespace removed

--- src/Input.php
+++ src/Input.php
@@ -14,18 +14,15 @@ class Input
         return $this->value;
     }
 
-
     public function setValue(Money $value): void
     {
         $this->value = $value;
     }
 
-
     public function getTransaction(): ?string
     {
         return $this->transaction;
     }
-
 
     public function setTransaction(string $transaction): void
     {
